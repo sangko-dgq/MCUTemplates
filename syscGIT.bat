7b974acece54613f3bf2cef58154b741e207d369
sangko-dgq <2201225826@qq.com>
2023-09-05 09:32:01 +0800

    cmdt2

--- syscGIT.bat
+++ syscGIT.bat
@@ -20,15 +20,40 @@ echo.
 echo =======================================================
 
 
+
 set /p choice="请选择操作："
 
 if "%choice%"=="1" (
+    call :check_git_repo
+    if !git_repo!==false (
+        echo 当前目录不是一个 Git 仓库，请进入正确的目录后再试.
+        pause
+        goto :menu
+    )
     call :add_files
 ) else if "%choice%"=="2" (
+    call :check_git_repo
+    if !git_repo!==false (
+        echo 当前目录不是一个 Git 仓库，请进入正确的目录后再试.
+        pause
+        goto :menu
+    )
     call :commit_files
 ) else if "%choice%"=="3" (
+    call :check_git_repo
+    if !git_repo!==false (
+        echo 当前目录不是一个 Git 仓库，请进入正确的目录后再试.
+        pause
+        goto :menu
+    )
     call :push_files
 ) else if "%choice%"=="4" (
+    call :check_git_repo
+    if !git_repo!==false (
+        echo 当前目录不是一个 Git 仓库，请进入正确的目录后再试.
+        pause
+        goto :menu
+    )
     call :one_key_push
 ) else if "%choice%"=="5" (
     goto :exit
@@ -38,6 +63,11 @@ if "%choice%"=="1" (
     goto :menu
 )
 
+:check_git_repo
+set git_repo=false
+if exist "%cd%\.git" set git_repo=true
+goto :eof
+
 :add_files
 echo.
 echo 正在添加文件到暂存区...
@@ -45,7 +75,7 @@ echo 正在添加文件到暂存区...
 git add .
 echo.
 echo 文件已成功添加到暂存区.
-pause
+call :Delay_1s
     goto :menu
 
 :commit_files
@@ -60,7 +90,7 @@ if "%commit_message%"=="" (
     git commit -m "%commit_message%"
     echo.
     echo 更改已成功提交到本地仓库.
-    pause
+call :Delay_1s
     goto :menu
 )
 
@@ -116,6 +146,11 @@ call :push_files
 
 pause
     goto :menu
+
+:Delay_1s
+echo.
+timeout /t 1 /nobreak >nul
+goto :eof
 
 :exit
 exit /b
